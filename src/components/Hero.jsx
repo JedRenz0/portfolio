e73f66b4087e0c5f41480f3142b21ab82472129b
Jed Villapando <jvillapa@uoguelph.ca>
2025-09-08 11:04:07 -0400

    feature: added banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,17 @@
+import banner from '../assets/sky.jpg';
+
+
 export default function Hero(){
     return(
-        <section id="home">
-            <h1>Hi, I'm Jed</h1>
-            <p>Computer Engineering student and aspiring software developer.</p>
-            <a href="#projects">See my work</a>
+        <section id="home" className="hero">
+            <div className="hero-image">
+                <img src={banner} alt="Banner"/>
+            </div>
+            <div className="hero-text">
+                <h1>Hi, I'm Jed</h1>
+                <p>Computer Engineering student and aspiring software developer.</p>
+                <a href="#projects">See my work</a>
+            </div>
         </section>
     );
 }
