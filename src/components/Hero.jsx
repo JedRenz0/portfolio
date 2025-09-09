8ce0e8288725b4f3e795fc3e1d09526c09711048
Jed Villapando <jvillapa@uoguelph.ca>
2025-09-08 21:10:34 -0400

    features: added type react to hero page and worked on navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,34 @@
-import banner from '../assets/sky.jpg';
+import React from "react";
+import {ReactTyped} from "react-typed";
+import banner from "../assets/sky.jpg";
 
+export default function Hero() {
+  return (
+    <section id="home" className="hero">
+      <div className="hero-container">
+        <img
+            className="hero-img"
+            src={banner} 
+            alt="Sky banner"
+            loading="eager"
+        />
+      </div>
 
-export default function Hero(){
-    return(
-        <section id="home" className="hero">
-            <div className="hero-image">
-                <img src={banner} alt="Banner"/>
-            </div>
-            <div className="hero-text">
-                <h1>Hi, I'm Jed</h1>
-                <p>Computer Engineering student and aspiring software developer.</p>
-                <a href="#projects">See my work</a>
-            </div>
-        </section>
-    );
+      <div className="hero-text">
+        <h1>
+            Hi, I'm Jed and I like{" "}
+          <ReactTyped
+            strings={[" Programming", " Computing", " Electronics", " Music"]}
+            typeSpeed={100}
+            backSpeed={60}
+            backDelay={900}
+            smartBackspace
+            loop
+          />
+        </h1>
+        <p>Computer Engineering student and aspiring software developer.</p>
+        <a href="#projects">See my work</a>
+      </div>
+    </section>
+  );
 }
