5c106f6c75dcd6e6cf8448ac4ae1a5685c92fd58
Jed Villapando <jvillapa@uoguelph.ca>
2025-09-11 11:12:02 -0400

    feat: added more details to the hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,17 +16,30 @@ export default function Hero() {
 
       <div className="hero-text">
         <h1>
-            Hi, I'm Jed and I like{" "}
-          <ReactTyped
-            strings={[" Programming", " Computing", " Electronics", " Music"]}
+            Hi, I'm <span className="highlight">Jed Renzo</span>
+        </h1>
+        <h2>
+            I am a {""}
+        <ReactTyped
+            strings={[" Computer Engineer", " Programmer", " Experienced QA Tester", " Developer"]}
             typeSpeed={100}
             backSpeed={60}
             backDelay={900}
             smartBackspace
             loop
           />
-        </h1>
+
+        </h2>
         <p>Computer Engineering student and aspiring software developer.</p>
+        <list>
+            <li>Python</li>
+            <li>JavaScript</li>
+            <li>C/C++</li>
+            <li>HTML/CSS</li>
+            <li>React</li>
+            <li>Node.js</li>
+            <li>Git</li>
+        </list>
         <a href="#projects">See my work</a>
       </div>
     </section>
