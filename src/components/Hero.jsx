7359826fb3133f4b64f6a65c46ad5e31227045b1
Jed Villapando <jvillapa@uoguelph.ca>
2025-09-11 13:40:00 -0400

    feat: added profile image and tweaked the react typing prompts

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {ReactTyped} from "react-typed";
 import banner from "../assets/sky.jpg";
+import profile from "../assets/profile.png";
 
 export default function Hero() {
   return (
@@ -13,34 +14,39 @@ export default function Hero() {
             loading="eager"
         />
       </div>
+      <div className="hero-wrap">
+        <div className="hero-text">
 
-      <div className="hero-text">
-        <h1>
-            Hi, I'm <span className="highlight">Jed Renzo</span>
-        </h1>
-        <h2>
-            I am a {""}
-        <ReactTyped
-            strings={[" Computer Engineer", " Programmer", " Experienced QA Tester", " Developer"]}
-            typeSpeed={100}
-            backSpeed={60}
-            backDelay={900}
-            smartBackspace
-            loop
-          />
+            <h1>
+                Hi, I'm <span className="highlight">Jed Renzo</span>
+            </h1>
+            <h2>
+                {""}
+            <ReactTyped
+                strings={[" An Computer Engineering Student", " A Programmer", " An Experienced QA Tester", "A Developer"]}
+                typeSpeed={100}
+                backSpeed={60}
+                backDelay={900}
+                smartBackspace
+                loop
+            />
 
-        </h2>
-        <p>Computer Engineering student and aspiring software developer.</p>
-        <list>
-            <li>Python</li>
-            <li>JavaScript</li>
-            <li>C/C++</li>
-            <li>HTML/CSS</li>
-            <li>React</li>
-            <li>Node.js</li>
-            <li>Git</li>
-        </list>
-        <a href="#projects">See my work</a>
+            </h2>
+            <p>I am a Aspiring software developer with a strong foundation in computer engineering and a drive to create impactful technology.</p>
+            <list>
+                <li>Python</li>
+                <li>JavaScript</li>
+                <li>C/C++</li>
+                <li>HTML/CSS</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>Git</li>
+            </list>
+            <a href="#projects">See my work</a>
+        </div>
+        <div className="hero-photo">
+            <img src={profile} alt="Jed Renzo portrait" className="profile-img"/>
+        </div>
       </div>
     </section>
   );
