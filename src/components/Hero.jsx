c7a5eef17f0535fc8c65aafdb832ca5408b8742b
Jed Villapando <jvillapa@uoguelph.ca>
2025-09-11 14:18:35 -0400

    updt:added more information in the hero page and did styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,39 +15,39 @@ export default function Hero() {
         />
       </div>
       <div className="hero-wrap">
-        <div className="hero-text">
+            <div className="hero-text">
 
-            <h1>
-                Hi, I'm <span className="highlight">Jed Renzo</span>
-            </h1>
-            <h2>
-                {""}
-            <ReactTyped
-                strings={[" An Computer Engineering Student", " A Programmer", " An Experienced QA Tester", "A Developer"]}
-                typeSpeed={100}
-                backSpeed={60}
-                backDelay={900}
-                smartBackspace
-                loop
-            />
+                <h1>
+                    Hi, I'm <span className="highlight">Jed Renzo</span>
+                </h1>
+                <h2>
+                    {""}
+                <ReactTyped
+                    strings={[" An Computer Engineering Student", " A Programmer", " An Experienced QA Tester", "A Developer"]}
+                    typeSpeed={100}
+                    backSpeed={60}
+                    backDelay={900}
+                    smartBackspace
+                    loop
+                />
 
-            </h2>
-            <p>I am a Aspiring software developer with a strong foundation in computer engineering and a drive to create impactful technology.</p>
-            <list>
-                <li>Python</li>
-                <li>JavaScript</li>
-                <li>C/C++</li>
-                <li>HTML/CSS</li>
-                <li>React</li>
-                <li>Node.js</li>
-                <li>Git</li>
-            </list>
-            <a href="#projects">See my work</a>
-        </div>
-        <div className="hero-photo">
-            <img src={profile} alt="Jed Renzo portrait" className="profile-img"/>
-        </div>
-      </div>
+                </h2>
+                <p>I am a Aspiring software developer with a strong foundation in computer engineering and a drive to create impactful technology.</p>
+                <list>
+                    <li>2nd year Computer Engineering Student at University of Guelph</li>
+                    <li>Previous Technical Systems Analyst at RBC T&O </li>
+                    <li>Incomming Software Developer RBC T&O Summer 2026</li>
+                    <li>Passionate in software development</li>
+                    <li>Past envolvments with design teams and Robotics competitions</li>
+                </list>
+                <a href="#projects">See my work</a>
+                </div>
+                <div className="hero-photo">
+                    <img src={profile} alt="Jed Renzo portrait" className="profile-img"/>
+                </div>
+            </div>
+        
+      
     </section>
   );
 }
